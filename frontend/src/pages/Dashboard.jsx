b89e4cfb06365bdd9b7d9132d49ddbb82598bdd0
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-08-06 23:27:32 +0530

    added goals form in dashborad

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 function Dashboard() {
   const navigate = useNavigate()
@@ -17,7 +18,13 @@ function Dashboard() {
   }, [user, navigate])
 
   return (
-    <div>Dashboard</div>
+    <>
+    <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <p>Goals Dashboard</p>
+    </section>
+    <GoalForm />
+    </>
   )
 }
 
